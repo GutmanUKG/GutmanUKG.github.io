e34ae51e4285885d3e4c6e09c6b0b40ba811d34f
Vladimir <vgutman95@mail.ru>
2021-02-25 16:59:30 +0600

    add Tiny Slider 2

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({   
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        // adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"> <img src="../icons/left.svg" alt="left"> </button>',
-        nextArrow: '<button type="button" class="slick-next"> <img src="../icons/right.svg" alt="right"></button>',
-        responsive:[
-            {
-                breakpoint: 768,
-                settings: {
-                  dots: true,
-                  arrows:false
-                }
-              },
-        ]
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({   
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         // adaptiveHeight: true,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         prevArrow: '<button type="button" class="slick-prev"> <img src="../icons/left.svg" alt="left"> </button>',
+//         nextArrow: '<button type="button" class="slick-next"> <img src="../icons/right.svg" alt="right"></button>',
+//         responsive:[
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                   dots: true,
+//                   arrows:false
+//                 }
+//               },
+//         ]
+//     });
+
+// });
+
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls:false,
+  nav:false
 
 });
+document.querySelector('.prev').addEventListener('click', function(){
+  slider.goTo('prev');
+})
+document.querySelector('.next').addEventListener('click', function(){
+  slider.goTo('next');
+})
