7e55cc3cdbc4fc0ee76f983c40ff16508cfc9759
Vladimir <vgutman95@mail.ru>
2021-02-27 16:03:46 +0600

    add modal script

--- js/script.js
+++ js/script.js
@@ -46,23 +46,6 @@ $(document).ready(function(){
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
   // link
-
-    // $('.catalog-item__link').each(function(i){
-    //     $(this).on('click', function(e){
-    //       e.preventDefault();
-    //       $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //       $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
-    //     })
-    // });
-
-    // $('.catalog-item_back').each(function(i){
-    //   $(this).on('click', function(e){
-    //     e.preventDefault();
-    //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
-    //   })
-  // });
-
   function toggleSlide(item){
     $(item).each(function(i){
       $(this).on('click', function(e){
@@ -74,4 +57,25 @@ $(document).ready(function(){
 };
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item_back');
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function(){
+      $('.overlay , #consultation' ).fadeIn('fast');
+    });
+    // close modal
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #thanks,#order').fadeOut('slow')
+    });
+
+   
+    //  modal
+    $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+        // Title in modal
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        // modal
+        $('.overlay, #order').fadeIn();
+      })
+    });
 });
