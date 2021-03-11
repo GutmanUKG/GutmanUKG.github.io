d7af5a5303f52ed821368174f7db60a9712c265f
Vladimir <vgutman95@mail.ru>
2021-03-11 14:08:49 +0600

    Добавлен новый проект

---
+++ redrift/js/slider.js
@@ -0,0 +1,112 @@
+
+    $('.slide-inner').slick({
+      infinite: true,
+      slidesToShow: 1,
+      autoplay:false,
+      arrows:true,
+      nextArrow: '<button type="button" class="next"></button>',
+      prevArrow: '<button type="button" class="prev"></button>',
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+  
+    $('.wraper_mobile_slid').slick({
+      infinite: true,
+      slidesToShow: 1,
+      autoplay:false,
+      arrows:true,
+      prevArrow: '<button class="mobile_prev"></button>',
+      nextArrow: '<button class="mobile_next"></button>'
+    });
+    
+
+    $('.body_slid').slick({
+      infinite: true,
+      slidesToShow: 1,
+      autoplay:true,
+      arrows:false,
+    });
+    $('.wraper_item_slide_team').slick({
+      infinite: true,
+      slidesToShow: 1,
+      autoplay:true,
+      arrows:true,
+      prevArrow: '<button class="mobile_prev_team"></button>',
+      nextArrow: '<button class="mobile_next_team"></button>'
+    });
+    $('.inner_content').slick({
+      infinite: true,
+      slidesToShow: 2,
+      autoplay:false,
+      arrows:true,
+      prevArrow: '<button class="prev_client"></button>',
+      nextArrow: '<button class="next_client"></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+
+    $('.clients_footer').slick({
+      infinite: true,
+      slidesToShow: 4,
+      autoplay:true,
+      arrows:false,
+    });
+   
+    
+     
+   
+    
+  
