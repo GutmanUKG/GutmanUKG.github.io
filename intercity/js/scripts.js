7772f7141a8f96a214b281932df81f80ffc9b921
Vladimir <vgutman95@mail.ru>
2021-05-17 18:00:20 +0600

    Правки

--- intercity/js/scripts.js
+++
@@ -1,9 +0,0 @@
-//
-// $(window).ready(()=>{
-//     $('.container-loader').fadeOut('slow', function (){})
-// })
-//
-// window.addEventListener('load', ()=>{
-//     let container = document.querySelector('.container-loader');
-//     container.style.display = 'none'
-// })
