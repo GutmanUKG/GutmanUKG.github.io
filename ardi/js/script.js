6e9584eedd72890e227707f150a1da6123fcc237
Vladimir <vgutman95@mail.ru>
2021-06-22 15:31:49 +0600

    add new project ardi

---
+++ ardi/js/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+    const anim_svg = new Vivus(
+        'animate_svg',
+        {
+            type: 'delayed',
+            duration:100
+        }
+    )
+
+    const anim_svg_two = new Vivus(
+        'animate_svg_two',
+        {
+            type: 'sync',
+            duration:150
+        }
+    )
+    const anim_svg_three = new Vivus(
+        'animate_svg_three',
+        {
+            type: 'sync',
+            duration:150
+        }
+    )
+    
+    const svg_four = new Vivus(
+        'svg_four',
+        {
+            type: 'sync',
+            duration:150
+        }
+    )
+
+    const svg_five = new Vivus(
+        'svg_five',
+        {
+            type: 'sync',
+            duration:150
+        }
+    )
+
+    const svg_six = new Vivus(
+        'svg_six',
+        {
+            type: 'sync',
+            duration:150
+        }
+    )
+    const svg_seven = new Vivus(
+        'svg_seven',
+        {
+            type: 'sync',
+            duration:150
+        }
+    )
+})
+
