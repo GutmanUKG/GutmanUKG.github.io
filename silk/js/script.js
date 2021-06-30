1ec988a3fea5417a41221ed4fab4b51ca4331b62
Vladimir <vgutman95@mail.ru>
2021-06-30 16:21:09 +0600

    adptiv

--- silk/js/script.js
+++ silk/js/script.js
@@ -1,126 +1,133 @@
-'use strict';
-
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', ()=>{
     // UPDATE: I was able to get this working again... Enjoy!
 
-    var cursor = document.querySelector('.cursor');
-    var cursorinner = document.querySelector('.cursor2');
-    var a = document.querySelectorAll('a');
-    var items = document.querySelectorAll('.cb-splitshow-preview');
-    var text = cursor.querySelector('span');
-    var x = void 0;
+        var cursor = document.querySelector('.cursor');
+        var cursorinner = document.querySelector('.cursor2');
+        var a = document.querySelectorAll('a');
+        let items = document.querySelectorAll('.cb-splitshow-preview');
+        let text = cursor.querySelector('span');
+        let x;
 
-    items.forEach(function (item) {
-        console.log(item.dataset.cursorText);
-        item.addEventListener('mouseover', function (e) {
-            text.textContent = item.dataset.cursorText;
-            cursor.classList.add('' + item.dataset.cursor);
 
-            //    var x = e.clientX;
-            //    var y = e.clientY;
-            //    let cursorPos = Math.round(cursor.getBoundingClientRect().x);
-            //    let mousePos = e.clientX;
+        items.forEach(item=>{
+            console.log(item.dataset.cursorText);
+            item.addEventListener('mouseover', (e)=>{
+               text.textContent = item.dataset.cursorText;
+               cursor.classList.add(`${item.dataset.cursor}`)
+            })
+            item.addEventListener('mouseout', ()=>{
+                text.textContent = '';
+                cursor.classList.remove(`${item.dataset.cursor}`)
+            })
+        })
+        document.addEventListener('mousemove', function(e){
+            var x = e.clientX;
+            var y = e.clientY;
+            let cursorPosX = Math.round(cursor.getBoundingClientRect().x);
+            let cursorPosY = Math.round(cursor.getBoundingClientRect().y);
+            let mousePosX = e.clientX;
+            let mousePosY = e.clientY;
+               let resultPosX = (mousePosX / cursorPosX);
+               let resultPosY = (cursorPosY / mousePosY);
+               if(resultPosY < 0.5){
+                   resultPosY = 0.5;
+               }
+               if(resultPosY > 1.9){
+                   resultPosY = 1.8;
+               }
+               if(resultPosX < 1){
+                   resultPosX = 1;
+               }
+               if(resultPosX > 2){
+                   resultPosX = 2;
+               }
+               let translatePos = `translate(calc(${e.clientX}px - 50%)`;
+               let calcPos = `calc(${e.clientY}px - 50%)`;
+               let rotatePos = `rotate(${resultPosX}deg`;
+               let scalePos = `scale(${resultPosX}, ${resultPosY}`;
 
-            //    let resultPosX = (mousePos / cursorPos);
-            //    let resultPosY = (cursorPos / mousePos);
-            //    if(resultPosY < 1){
-            //        resultPosY = 1;
-            //    }
-            //    if(resultPosY > 2){
-            //        resultPosY = 2;
-            //    }
-            //    if(resultPosX < 1){
-            //        resultPosX = 1;
-            //    }
-            //    if(resultPosX > 2){
-            //        resultPosX = 2;
-            //    }
-            //    let activePos =  `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%)) rotate(${resultPosX}deg) scale(${resultPosX}, ${resultPosY})`;
-            //    let passivePos  =  activePos;
-            //    console.log(passivePos)
-            //    cursor.style.transform = `${activePos}`;
-            //    function end(){
-            //        resultPosX = 1;
-            //        resultPosY = 1;
+           
 
-            //    }
-            //    if (x) clearTimeout(x); 
-            //    x = setTimeout(end, 400); 
+
+               let activePos =  `${translatePos}, ${calcPos}) ${rotatePos}) ${scalePos})`;
+              
+               
+               let test = activePos.match(/scale/);
+         
+               cursor.style.transform = `${activePos}`;
+
+               function end(){
+                // resultPosX = 1;
+                // resultPosY = 1;
+                // scalePos = `scale(${resultPosX}, ${resultPosY}`
+                // let passivePos  = `${translatePos}, ${calcPos}) ${rotatePos}) ${scalePos})`;
+                // cursor.style.transform = `${passivePos}`
+               }
+               if (x) clearTimeout(x); 
+               x = setTimeout(end, 500); 
         });
-        item.addEventListener('mouseout', function () {
-            text.textContent = '';
-            cursor.classList.remove('' + item.dataset.cursor);
+      
+        document.addEventListener('mousemove', function(e){
+            var x = e.clientX;
+            var y = e.clientY;
+            cursorinner.style.left = x + 'px';
+            cursorinner.style.top = y + 'px';
+
+            
         });
-    });
-    document.addEventListener('mousemove', function (e) {
-        var x = e.clientX;
-        var y = e.clientY;
-        var cursorPosX = Math.round(cursor.getBoundingClientRect().x);
-        var cursorPosY = Math.round(cursor.getBoundingClientRect().y);
-        var mousePosX = e.clientX;
-        var mousePosY = e.clientY;
-        // cursor.style.top = `${e.clientY}px`;
-        // cursor.style.left = `${e.clientX}px`
-        var resultPosX = mousePosX / cursorPosX;
-        var resultPosY = cursorPosY / mousePosY;
-        if (resultPosY < 0.5) {
-            resultPosY = 0.5;
-        }
-        if (resultPosY > 1.9) {
-            resultPosY = 1.8;
-        }
-        if (resultPosX < 1) {
-            resultPosX = 1;
-        }
-        if (resultPosX > 2) {
-            resultPosX = 2;
-        }
-        var translatePos = 'translate(calc(' + e.clientX + 'px - 50%)';
-        var calcPos = 'calc(' + e.clientY + 'px - 50%)';
-        var rotatePos = 'rotate(' + resultPosX + 'deg';
-        var scalePos = 'scale(' + resultPosX + ', ' + resultPosY;
 
-        var activePos = translatePos + ', ' + calcPos + ') ' + rotatePos + ') ' + scalePos + ')';
+        document.addEventListener('mousedown', function(){
+            cursor.classList.add('click');
+            cursorinner.classList.add('cursorinnerhover')
+        });
 
-        console.log(activePos);
-        var test = activePos.match(/scale/);
-        console.log('dfd' + test);
-        cursor.style.transform = '' + activePos;
+        document.addEventListener('mouseup', function(){
+            cursor.classList.remove('click')
+            cursorinner.classList.remove('cursorinnerhover')
+        });
 
-        function end() {
-            // resultPosX = 1;
-            // resultPosY = 1;
-            // scalePos = `scale(${resultPosX}, ${resultPosY}`
-            // let passivePos  = `${translatePos}, ${calcPos}) ${rotatePos}) ${scalePos})`;
-            // cursor.style.transform = `${passivePos}`
-        }
-        if (x) clearTimeout(x);
-        x = setTimeout(end, 500);
-    });
-
-    document.addEventListener('mousemove', function (e) {
-        var x = e.clientX;
-        var y = e.clientY;
-        cursorinner.style.left = x + 'px';
-        cursorinner.style.top = y + 'px';
-    });
-
-    document.addEventListener('mousedown', function () {
-        cursor.classList.add('click');
-        cursorinner.classList.add('cursorinnerhover');
-    });
-
-    document.addEventListener('mouseup', function () {
-        cursor.classList.remove('click');
-        cursorinner.classList.remove('cursorinnerhover');
-    });
-
-    a.forEach(function (item) {
-        item.addEventListener('mouseover', function () {
+        a.forEach(item => {
+        item.addEventListener('mouseover', () => {
             cursor.classList.add('hover');
         });
-        item.addEventListener('mouseleave', function () {
+        item.addEventListener('mouseleave', () => {
             cursor.classList.remove('hover');
         });
-    });
-});
+    })
+
+    // Magic animation
+
+
+    // let silkBg = document.querySelector('.silk_bg');
+    // let rowBg = document.createElement('div');
+    // rowBg.classList.add('row_bg');
+    
+
+    // let countRow = silkBg.scrollHeight / 50;
+    // let countWidth = silkBg.clientWidth / 50;
+    // console.log(countRow)
+    // console.log(countWidth)
+    // for(let i = 0; i <= Math.round(countRow); i++){ 
+    //     silkBg.appendChild(rowBg.cloneNode(true))
+    // }
+    // let ax = Math.round(countWidth);
+    // let by = Math.round(countRow)
+    // let randomScale = function(ax , by){
+    //     return Math.random() * (Math.round(ax - by)- Math.round(ax + by))
+    // }
+    // console.log(randomScale(ax, by))
+    // silkBg.addEventListener('click', ()=>{
+    //     let rows = document.querySelectorAll('.row_bg');
+
+    //     function anim(){
+    //         for(let i = 0; i <= rows.length; i++) {
+    //             let posScale = Math.round(randomScale(ax, by));
+    //             rows[i].style.transform = `translate(100%, 0%) rotate(${posScale}deg)`;
+    //             rows[i].style.transition = (i *100) +'ms';
+    //         }
+    //     }
+    //     rows.forEach(item => {
+    //         item.addEventListener('click', anim())
+    //     })
+    // })
+})
