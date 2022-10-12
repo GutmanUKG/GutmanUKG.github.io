0fbc5b5367afb946152787621d55e63d6f10602b
Vladimir <vgutman95@mail.ru>
2022-10-12 12:15:08 +0600

    'add_new_page'

--- businesswomen/assets/js/scripts.js
+++ businesswomen/assets/js/scripts.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    try{
+        const tabsContentListItem = document.querySelectorAll('.tabs_content_list-item'),
+            tabsBtnListItem = document.querySelectorAll('.tabs_btn_list-item');
 
+        tabsBtnListItem[0].classList.add('tabs_btn_list-item--active')
+        tabsContentListItem[0].classList.add('tabs_content_list-item--active')
+
+        function clearClass(elements, activeClass){
+            for(let i = 0; i < elements.length; i++){
+                elements[i].classList.remove(activeClass)
+            }
+        }
+        tabsBtnListItem.forEach((item,id)=>{
+            item.addEventListener('click', ()=>{
+                clearClass(tabsBtnListItem, 'tabs_btn_list-item--active')
+                item.classList.add('tabs_btn_list-item--active')
+                clearClass(tabsContentListItem, 'tabs_content_list-item--active')
+                tabsContentListItem[id].classList.add('tabs_content_list-item--active')
+            })
+        })
+    }catch(e){
+
+    }
 });
--- businesswomen/js/scripts.js
+++ businesswomen/js/scripts.js
@@ -1,4 +1,25 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  try {
+    var clearClass = function clearClass(elements, activeClass) {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove(activeClass);
+      }
+    };
+
+    var tabsContentListItem = document.querySelectorAll('.tabs_content_list-item'),
+        tabsBtnListItem = document.querySelectorAll('.tabs_btn_list-item');
+    tabsBtnListItem[0].classList.add('tabs_btn_list-item--active');
+    tabsContentListItem[0].classList.add('tabs_content_list-item--active');
+    tabsBtnListItem.forEach(function (item, id) {
+      item.addEventListener('click', function () {
+        clearClass(tabsBtnListItem, 'tabs_btn_list-item--active');
+        item.classList.add('tabs_btn_list-item--active');
+        clearClass(tabsContentListItem, 'tabs_content_list-item--active');
+        tabsContentListItem[id].classList.add('tabs_content_list-item--active');
+      });
+    });
+  } catch (e) {}
+});
 //# sourceMappingURL=scripts.js.map
