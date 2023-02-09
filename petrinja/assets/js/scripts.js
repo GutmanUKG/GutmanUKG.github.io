192250c3bdc8bbda55b3f1e162ab73af11ebe5ac
Vladimir <vgutman95@mail.ru>
2023-02-09 09:28:02 +0600

    'p'

--- petrinja/assets/js/scripts.js
+++ petrinja/assets/js/scripts.js
@@ -3,13 +3,58 @@ document.addEventListener('DOMContentLoaded', ()=>{
         container: '.cards-list',
         items: 3,
         controls:false,
-        nav:false
+        nav:false,
+        responsive: {
+            1001: {
+                items: 3
+            },
+            768:{
+                items: 2.2,
+                loop: false
+            },
+            0: {
+                items: 1.2,
+                loop: false
+            }
+
+        }
     });
     const NewsSlider = tns({
         container: '.news-list',
         items: 3,
         controls:false,
         nav:false,
-        mouseDrag: true
+        mouseDrag: true,
+        responsive: {
+            1001: {
+                items: 3
+            },
+            768:{
+                items: 2.2,
+                loop: false
+            },
+            0: {
+                items: 1.2,
+                loop: false
+            }
+
+        }
     });
+
+    const overlay = document.querySelector('.overlay');
+    const burgerBtn = document.querySelector('.header-burger'),
+        burgerMenu = document.querySelector('.burger-menu'),
+        burgerClose = burgerMenu.querySelector('.burger-close');
+
+    burgerBtn.addEventListener('click', ()=>{
+        burgerMenu.classList.add('active')
+        overlay.classList.add('active')
+    })
+    burgerClose.addEventListener('click', ()=>{
+        burgerMenu.classList.remove('active')
+    })
+    overlay.addEventListener('click', ()=>{
+        burgerMenu.classList.remove('active')
+        overlay.classList.remove('active')
+    })
 });
--- petrinja/js/scripts.js
+++ petrinja/js/scripts.js
@@ -5,14 +5,55 @@ document.addEventListener('DOMContentLoaded', function () {
     container: '.cards-list',
     items: 3,
     controls: false,
-    nav: false
+    nav: false,
+    responsive: {
+      1001: {
+        items: 3
+      },
+      768: {
+        items: 2.2,
+        loop: false
+      },
+      0: {
+        items: 1.2,
+        loop: false
+      }
+    }
   });
   var NewsSlider = tns({
     container: '.news-list',
     items: 3,
     controls: false,
     nav: false,
-    mouseDrag: true
+    mouseDrag: true,
+    responsive: {
+      1001: {
+        items: 3
+      },
+      768: {
+        items: 2.2,
+        loop: false
+      },
+      0: {
+        items: 1.2,
+        loop: false
+      }
+    }
+  });
+  var overlay = document.querySelector('.overlay');
+  var burgerBtn = document.querySelector('.header-burger'),
+      burgerMenu = document.querySelector('.burger-menu'),
+      burgerClose = burgerMenu.querySelector('.burger-close');
+  burgerBtn.addEventListener('click', function () {
+    burgerMenu.classList.add('active');
+    overlay.classList.add('active');
+  });
+  burgerClose.addEventListener('click', function () {
+    burgerMenu.classList.remove('active');
+  });
+  overlay.addEventListener('click', function () {
+    burgerMenu.classList.remove('active');
+    overlay.classList.remove('active');
   });
 });
 //# sourceMappingURL=scripts.js.map
