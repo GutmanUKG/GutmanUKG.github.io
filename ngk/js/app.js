80e1d8b6443b1009deb6b24b8789afb6b33578e6
Vladimir Gutman <gutman@oneit.kz>
2023-11-02 12:05:20 +0600

    fix scroll

--- ngk/js/app.js
+++ ngk/js/app.js
@@ -5,83 +5,128 @@ document.addEventListener("DOMContentLoaded", function () {
     burgerBtn = document.querySelector(".btn-burger"),
     burgerMenu = document.querySelector(".header-info"),
     closeBurgerMenuBtn = burgerMenu.querySelector(".header-btn--close");
+  var overlay = document.querySelector(".overlay_body");
   burgerBtn.addEventListener("click", function () {
     burgerMenu.classList.add("active");
   });
   closeBurgerMenuBtn.addEventListener("click", function () {
     burgerMenu.classList.remove("active");
   });
-  var servicesWrapper = document.querySelector(".services-list--overflow");
-  servicesWrapper.style.cssText = "width:".concat(document.body.clientWidth, "px; \n  min-width: ").concat(document.body.clientWidth, "px; overflow-x: hidden");
-  new Vue({
-    el: "#app_vue",
-    data: function data() {
-      return {
-        sectionInViewport: false,
-        element: document.querySelector(".services-list"),
-        cnt: 0,
-        styleObject: "",
-        isScroll: false
-      };
-    },
-    mounted: function mounted() {
-      var options = {
-        root: null,
-        // Используем viewport как корневой элемент
-        rootMargin: "0px",
-        // Может быть настроено для марджинов
-        threshold: 0.7 // Порог видимости (от 0 до 1)
-      };
+  //Все для автоскролла в секции услуги
+  var servicesList = document.querySelector(".services-list");
+  var items = servicesList.querySelectorAll(".item");
+  if (document.body.clientWidth > 1023) {
+    var itemsWidth = 0;
+    for (var i = 0; i < items.length; i++) {
+      itemsWidth += parseInt(items[i].clientWidth);
+    }
+    var servicesWrapper = document.querySelector(".services-list--overflow");
+    servicesWrapper.style.cssText = "width:".concat(document.body.clientWidth, "px; \n    min-width: ").concat(document.body.clientWidth, "px; overflow-x: hidden");
+    new Vue({
+      el: "#app_vue",
+      data: function data() {
+        return {
+          sectionInViewport: false,
+          element: document.querySelector(".services-list"),
+          cnt: 0,
+          styleObject: "",
+          isScroll: false,
+          singleItemWidth: itemsWidth / items.length,
+          fullItemsWidth: itemsWidth - items[0].clientWidth,
+          persentItemWidth: Math.floor((items[0].clientWidth + 16) / (itemsWidth + 16 * items.length) * 100 - 3),
+          maxPersentTransparent: 100
+        };
+      },
+      mounted: function mounted() {
+        var options = {
+          root: null,
+          // Используем viewport как корневой элемент
+          rootMargin: "0px",
+          // Может быть настроено для марджинов
+          threshold: 0.7 // Порог видимости (от 0 до 1)
+        };
 
-      var observer = new IntersectionObserver(this.handleIntersection, options);
-      observer.observe(this.$refs.sectionToTrack);
-      document.body.clientWidth > 767 ? document.addEventListener("wheel", this.onScroll) : "";
-    },
-    methods: {
-      handleIntersection: function handleIntersection(entries) {
-        // Вызывается при изменении видимости
-        if (entries[0].isIntersecting) {
-          this.sectionInViewport = true;
-          // Секция находится в зоне видимости
-          console.log(this.sectionInViewport);
-          // this.isScroll ? body.classList.add("horizontal") : "";
-        } else {
-          this.sectionInViewport = false;
-          // Секция не находится в зоне видимости
-          console.log(this.sectionInViewport);
-          // body.classList.remove("horizontal");
-          // this.styleObject = "transform: translate(" + 0 + "%)";
+        var observer = new IntersectionObserver(this.handleIntersection, options);
+        observer.observe(this.$refs.sectionToTrack);
+        document.body.clientWidth > 767 ? document.addEventListener("wheel", this.onScroll) : "";
+      },
+      methods: {
+        handleIntersection: function handleIntersection(entries) {
+          // Вызывается при изменении видимости
+          if (entries[0].isIntersecting) {
+            this.sectionInViewport = true;
+            // Секция находится в зоне видимости
+            console.log(this.sectionInViewport);
+          } else {
+            this.sectionInViewport = false;
+            // Секция не находится в зоне видимости
+            console.log(this.sectionInViewport);
+          }
+        },
+        calcMaxDown: function calcMaxDown() {},
+        onScroll: function onScroll(event) {
+          event = event || window.event;
+          var y = event.deltaY || event.detail || event.wheelDelta,
+            val = 0.1,
+            min = 0,
+            max = 100;
+          if (y > 0 && this.sectionInViewport && -this.cnt < this.fullItemsWidth) {
+            this.cnt = this.cnt - this.singleItemWidth;
+            body.classList.add("horizontal");
+            this.styleObject = "transform: translate(" + this.cnt + "px)";
+          }
+          if (y < 0 && this.sectionInViewport && (this.ctn >= 0 || this.cnt != 0)) {
+            this.cnt = this.cnt + this.singleItemWidth;
+            console.log("up");
+            this.styleObject = "transform: translate(" + this.cnt + "px)";
+          } else {
+            body.classList.remove("horizontal");
+          }
         }
       },
-      calcMaxDown: function calcMaxDown() {},
-      onScroll: function onScroll(event) {
-        event = event || window.event;
-        var y = event.deltaY || event.detail || event.wheelDelta,
-          val = 0.1,
-          min = 0,
-          max = 100;
-        console.log(this.sectionInViewport ? "видно" : "Не видно");
-        if (y > 0 && this.sectionInViewport && this.cnt >= -55) {
-          this.cnt = this.cnt - 5;
-          console.log("down");
-          body.classList.add("horizontal");
-          this.styleObject = "transform: translate(" + this.cnt + "%)";
-        }
-        if (y < 0 && this.sectionInViewport && (this.ctn >= 0 || this.cnt != 0)) {
-          this.cnt = this.cnt + 5;
-          console.log("up");
-          this.styleObject = "transform: translate(" + this.cnt + "%)";
-        } else {
-          body.classList.remove("horizontal");
+      watch: {
+        cnt: function cnt(newVal, oldVal) {
+          newVal == 0 ? body.classList.remove("horizontal") : "";
+          newVal == -190 ? body.classList.remove("horizontal") : body.classList.add("horizontal");
         }
       }
-    },
-    watch: {
-      cnt: function cnt(newVal, oldVal) {
-        newVal == 0 ? body.classList.remove("horizontal") : "";
-        newVal == -190 ? body.classList.remove("horizontal") : body.classList.add("horizontal");
+    });
+  } else {
+    servicesList.classList.add("owl-carousel", "owl-theme");
+    var servicePopup = document.querySelector(".service_popup"),
+      servicePopupClose = servicePopup.querySelector(".close_btn");
+    $(".services-list").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: {
+          items: 1.2
+        },
+        600: {
+          items: 2.2
+        }
       }
-    }
-  });
+    });
+    items.forEach(function (i) {
+      i.addEventListener("click", function () {
+        var title = i.querySelector("h2"),
+          descr = i.querySelector(".item-descr"),
+          img = i.querySelector(".img img");
+        servicePopup.querySelector(".service_popup-title").textContent = title.textContent;
+        servicePopup.querySelector(".service_popup-top").style.cssText = "background: url(".concat(img.src, ")center/cover no-repeat");
+        servicePopup.querySelector(".service_popup-body").innerHTML = descr.innerHTML;
+        servicePopup.classList.add("active");
+        overlay.classList.add("active");
+        body.style.overflow = "hidden";
+      });
+    });
+    servicePopupClose.addEventListener("click", function () {
+      servicePopup.classList.remove("active");
+      overlay.classList.remove("active");
+      body.style.overflow = "";
+    });
+  }
 });
 //# sourceMappingURL=app.js.map
